0261d4ea019fdf6b3d416c61af73ac6b5e35f601
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2024-07-22 14:42:55 +0700

    update permision

--- BookStore/Service/JwtTokenService.cs
+++ BookStore/Service/JwtTokenService.cs
@@ -24,7 +24,7 @@ namespace BookStore.Service
             if (user.RoleId == 1)
             {
                 role = "Admin";
-            }
+            }else
             {
                 role = "Customer";
             }
