b51afeafbfcb568ec9e1b933c41cb21ee9e8775b
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2024-07-25 01:06:53 +0700

    test

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -104,12 +104,14 @@ builder.Services.ConfigureServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
 }
-
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
